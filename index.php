3e1e0df7f3731fd1d3ce287ff511cc1fd5169157
baptistestubljar <baptiste.stubljar@gmail.com>
2021-11-16 08:35:04 +0100

    Préparation base html

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hydro - Acceuil</title>
+</head>
+<body>
+    <?php include "./modules/header.php" ?>
+    <section class="main-image">
+        <h1>Regardez des films avec Hydro</h1>
+        <img src="./img/banniere.jpg" alt="banniere acceuil">
+    </section>
+    <section class="main-image">
+        <h2>Des centaines de films séléctionnés pour vous</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste similique saepe officiis impedit illo, ducimus minus quisquam iusto fugiat. Quo dolores nihil omnis, voluptatem itaque eum quisquam quaerat commodi temporibus?</p>
+    </section>
+    <section class="main-image">
+        <h2>Un accès partout, tout le temps</h2>
+        <img src="./img/left_banniere.jpg" alt="">
+    </section>
+    
+    
+    
+</body>
+</html>
