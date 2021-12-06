49d9ba0fbbec8ca08392bfab9d1150351a2853c0
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2021-12-06 20:15:10 +0100

    test

--- js/script.js
+++ js/script.js
@@ -10,18 +10,9 @@ const API_KEY = 'api_key=0cbdab6dfb9d5c3d8b7a3cf506e11b83'
 const BASE_URL = 'https://api.themoviedb.org/3/search/movie?'
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/'
 
+//test();
 showMovies();
 
-function getMovie(url) {
-    let movie = new Array();
-    fetch(url).then(res => res.json()).then(data => { 
-        movie.push(data.results[0]['title']);
-        movie.push(data.results[0]['release_date']);
-        movie.push(data.results[0]['poster_path']);
-        movie.push(data.results[0]['overview']);
-    })    
-    return movie;
-}
 
 function showMovies() {
     let movies = document.getElementsByClassName("movie-tile");
@@ -32,15 +23,38 @@ function showMovies() {
         let localTitle = "Star Wars"
         let year = 2010;
         let url = BASE_URL + API_KEY + '&' + 'query=' + localTitle + '&' + 'year=' + year;
-
-        let movieInfo = getMovie(url);
-        console.log(movieInfo);
-        console.log(movieInfo[0]);
+        
+        getMovie(id, url);
         /*console.log(movieInfo['']);*/
-
+        /*
         movie.innerHTML = `
-            <img src="${IMG_URL+movieInfo['poster_path']}" alt="${movieInfo['title']}">
-            <figcaption>${movieInfo['title']} <br> ${movieInfo['release_date']}</figcaption>
-        `;
+        <img src="${IMG_URL+movieInfo['poster_path']}" alt="${movieInfo['title']}">
+        <figcaption>${movieInfo['title']} <br> ${movieInfo['release_date']}</figcaption>
+        `;*/
     }
+}
+
+function getMovie(id, url) {
+    let test;
+    fetch(url).then(res => res.json()).then(data => { 
+        test = data.results[0].poster_path;
+        updateMovie(id, test);
+    })  
+}
+
+function updateMovie(id, test) {
+    console.log(test);
+}
+
+function createObject(){
+    let o = {foo: null, bar: null};
+    o.foo = 5;
+    return o;
+}
+
+function test(){
+    let o = createObject();
+    o.bar = 1;
+    console.log(o.foo);
+    console.log(o.bar);
 }
