6e0c403d7f97534bbe40b67151f5c1a53f7bf5d2
dalilMAADOUR <83078884+dalil-dev@users.noreply.github.com>
2021-12-06 15:10:29 +0100

    petite modif

--- python/main.py
+++ python/main.py
@@ -54,7 +54,6 @@ if __name__ == '__main__':
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            print("INSERT INTO movies VALUES(?,?,?);",(row[0],row[1],row[2]))
             if line_count > 0:
                 cur.execute("INSERT INTO movies VALUES(?,?,?);",(row[0],row[1],row[2]))
             line_count+=1
