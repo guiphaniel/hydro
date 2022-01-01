de00124dfcbb658dd355a466a650ff99b4750edb
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-01 19:47:30 +0100

    not connected verification

--- catalogue.php
+++ catalogue.php
@@ -1,4 +1,9 @@
-<?php session_start(); ?>
+<?php session_start(); 
+    if (!isset($_SESSION['user'])) {
+        header('location: index.php');
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
