88846ed89b91c796fb5ccf0ef01ccbb5b24a13ae
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-01 18:06:30 +0100

    actors & genres

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+const API_KEY = 'api_key=0cbdab6dfb9d5c3d8b7a3cf506e11b83'
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie?'
+const IMG_URL = 'https://image.tmdb.org/t/p/w500/'
 let nbStars = 0;
 
-function showFilmDetails(title, year) {
+function showFilmDetails(title, year, genres) {
     document.getElementById('movie-details-container').classList.add('show');
 
     let content = document.getElementById('movie-details-content');
@@ -8,8 +11,25 @@ function showFilmDetails(title, year) {
     let url = BASE_URL + API_KEY + '&' + 'query=' + title + '&' + 'year=' + year;
 
     //TODO: initialiser le nbStars à la moyenne du film
-    getMovie(url).then(movieInfo => {
-        content.innerHTML = ` 
+    let actors = getMovie(url).then(movieInfo => {
+        //TODO: recuperer les acteurs
+        
+        getCredits(movieInfo.id).then(credits => {
+            let actorsHTML = ""
+            try {
+                for (let index = 0; index < 10; index++) {
+                    let actor = credits[index]   
+                    actorsHTML += `
+                        <div class="actor">                                
+                            <div class="actor-img" style="background-image: url('${IMG_URL+actor["profile_path"]}')" alt="${actor["name"]}"></div>
+                            <p class="actor-name">${actor["name"]} <br> ${actor["character"]}</p>
+                        </div>
+                        `
+                }
+            } catch(error) {
+
+            } finally {
+                content.innerHTML = ` 
             <span class="close" onclick="closeFilmDetails()">&times;</span>
             <div id="movie-details">
                 <div id="movie">                                
@@ -27,10 +47,9 @@ function showFilmDetails(title, year) {
                         <svg id="star-5" onclick="checkStar('star-5')"aria-hidden="true" focusable="false" data-prefix="fad" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="star"><g class="fa-group"><path fill="currentColor" d="M528.53 171.5l-146.36-21.3-65.43-132.39c-11.71-23.59-45.68-23.89-57.48 0L193.83 150.2 47.47 171.5c-26.27 3.79-36.79 36.08-17.75 54.58l105.91 103-25 145.49c-4.52 26.3 23.22 46 46.48 33.69L288 439.56l130.93 68.69c23.26 12.21 51-7.39 46.48-33.69l-25-145.49 105.91-103c19-18.49 8.48-50.78-17.79-54.57zm-90.89 71l-66.05 64.23 15.63 90.86a12 12 0 0 1-17.4 12.66L288 367.27l-81.82 42.94a12 12 0 0 1-17.4-12.66l15.63-90.86-66-64.23A12 12 0 0 1 145 222l91.34-13.28 40.9-82.81a12 12 0 0 1 21.52 0l40.9 82.81L431 222a12 12 0 0 1 6.64 20.46z" class="star-outer"></path><path fill="currentColor" d="M437.64 242.46l-66.05 64.23 15.63 90.86a12 12 0 0 1-17.4 12.66L288 367.27l-81.82 42.94a12 12 0 0 1-17.4-12.66l15.63-90.86-66-64.23A12 12 0 0 1 145 222l91.34-13.28 40.9-82.81a12 12 0 0 1 21.52 0l40.9 82.81L431 222a12 12 0 0 1 6.64 20.46z" class="star-inner"></path></g></svg>
                     </div>
                     <p>${movieInfo.overview}</p>
-                    <p>genres</p>
-                    <p>Realisateur</p>
+                    <p id="genres">${genres}</p>
                     <div id="actors">
-
+                        ${actorsHTML}
                     </div>
                 </div>
             </div>
@@ -38,6 +57,8 @@ function showFilmDetails(title, year) {
 
             </div> 
         `;
+            }
+        });
     });
 }
 
@@ -45,19 +66,25 @@ function closeFilmDetails() {
     document.getElementById('movie-details-container').classList.remove('show');
 }
 
-const API_KEY = 'api_key=0cbdab6dfb9d5c3d8b7a3cf506e11b83'
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie?'
-const IMG_URL = 'https://image.tmdb.org/t/p/w500/'
 
 function getMovie(url) {
     return fetch(url).then(res => res.json()).then(data => { 
-        let movie = {title: null, release_date: null, poster_path: null, overview: null}
+        let movie = {id: null, title: null, release_date: null, poster_path: null, overview: null}
+        movie.id = data.results[0]['id']
         movie.title = data.results[0]['title']
         movie.release_date = data.results[0]['release_date']
         movie.poster_path = data.results[0]['poster_path']
         movie.overview = data.results[0]['overview']        
         return movie 
     })  
+}
+
+function getCredits(id){
+    //let content = document.getElementById('movie-details-content');
+        
+    let url = "https://api.themoviedb.org/3/movie/" + id +"/credits?" + API_KEY;
+
+    return fetch(url).then(res => res.json()).then(data => data["cast"])
 }
  
 function showMovie(htmlId, title, year) {
