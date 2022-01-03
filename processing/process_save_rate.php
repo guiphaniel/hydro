d62b5b32ccad8428a63c854f5c6cdae194881e67
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-03 14:03:24 +0100

    remove useless userId

--- processing/process_save_rate.php
+++ processing/process_save_rate.php
@@ -1,5 +1,5 @@
 <?php
-    if(!(isset($_POST["idUser"]) && isset($_POST["idMovie"]) && isset($_POST["rating"]))) {
+    if(!(isset($_POST["idMovie"]) && isset($_POST["rating"]))) {
         header("location: ../catalogue.php");
         exit();
     }
@@ -7,13 +7,13 @@
     include "../include/start-db.php";
 
     //verification anti-hack
-    $sql = $pdo->prepare("SELECT * from movies where idMovie = :id");
+    $sql = $pdo->prepare("SELECT * from movies where idMovie = :id"); //on verifie l'existence du film
     $sql->execute([
         "id" => $_POST["idMovie"]
     ]);
     $movie = $sql->fetch();
 
-    if($_POST["idUser"] != $_SESSION['user']['id'] || !$movie || $_POST["rating"] < 0 || $_POST["rating"] > 5) {
+    if(!$movie || $_POST["rating"] < 0 || $_POST["rating"] > 5) {
         header("location: ../catalogue.php");
         exit();
     }
@@ -21,7 +21,7 @@
     //sauvegarde dans la bd
     $sql = $pdo->prepare("INSERT INTO ratings values (:idUser, :idMovie, :rating, :timestamp)"); //TODO: mettre la bonne table
     $sql->execute([
-        "idUser" => $_POST["idUser"],
+        "idUser" => $_SESSION['user']['id'],
         "idMovie" => $_POST["idMovie"],
         "rating" => number_format($_POST["rating"], 1),
         "timestamp" => time(),
