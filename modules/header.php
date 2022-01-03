df04b420b7b563b94182b39f73bab1966af91a86
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-03 14:09:31 +0100

    remove useless buttons (catalogue & index)

--- modules/header.php
+++ modules/header.php
@@ -4,16 +4,13 @@
     <a href="./index.php"><img id="logo-with-text" for="toogle" src="./img/LogoSombreTexte.png" alt="logo Hydro with text"></a>
     <img id="logo-without-text" for="toogle" src="./img/LogoSombre.svg" alt="logo Hydro">
     
-    <ul id="menu"class="accountButtons">
-        <li><div id= "homeBtn" class="button" onclick="location.href ='./index.php'">Acceuil</div></li>
-        
+    <ul id="menu"class="accountButtons">        
         <?php 
         if(empty($_SESSION['user'])){
             echo '<li><div id= "signUpBtn"class="button" onclick="show(modalSignUp)">Inscription</div></li>';
             echo '<li><div id="signInBtn" class="button" onclick="show(modalSignIn)">Connexion</div></li>';
         }
         else{
-            echo '<li><div id= "catalogueBtn" class="button" onclick="location.href =\'./catalogue.php\'">Catalogue</div></li>';
             echo '<li><div  id= "signOutBtn" class="button" onclick="location.href = \'./processing/process_sign_out.php\';" onclick="signOut();">Deconnexion</div></li>';
         }
         ?>
