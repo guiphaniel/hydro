6b4a1b8bb7bd45111d80e5f081962d1c4d4c74e3
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-04 22:05:37 +0100

    adding maito in "nous contacter"

--- modules/footer.php
+++ modules/footer.php
@@ -2,7 +2,7 @@
     <img src="img\LogoSombre.svg" alt="logo sombre sans texte">
     <ul>
         <li><a href="#">Conditions d'utilisation</a></li>
-        <li><a href="#">Nous contacter</a></li>
+        <li><a href="mailto:support@hydro.fr">Nous contacter</a></li>
         <li><a href="">Hydro 2021</a></li>
     </ul>
     
